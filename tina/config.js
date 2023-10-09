019aa1d53b3a2cf8d59980933389c030f66c6e80
theindy <theindy@gmail.com>
2023-10-08 20:32:35 -0700

    tokens

--- tina/config.js
+++ tina/config.js
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: "n", // Get this from tina.io
-  token: "n", // Get this from tina.io
+  clientId: "9c28524f-cd54-4cb7-b7f6-3cc0fcda37d3", // Get this from tina.io
+  token: "247d3c3a1c3e00bb1e2539a13763c6037692b7ee", // Get this from tina.io
 
   build: {
     outputFolder: "admin",
