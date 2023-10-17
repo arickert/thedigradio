f3683a9de418ca157dcf95af22f3cbf1b822e500
arickert <45617293+arickert@users.noreply.github.com>
2023-10-17 16:51:58 -0700

    Update config.js

--- tina/config.js
+++ tina/config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
-
+console.log('TINA_PUBLIC_CLIENT_ID:', process.env.TINA_PUBLIC_CLIENT_ID);
+console.log('TINA_TOKEN:', process.env.TINA_TOKEN);
+console.log('JEKYLL_ENV:', process.env.JEKYLL_ENV);
 export default defineConfig({
   branch,
   clientId: process.env.TINA_PUBLIC_CLIENT_ID, 
