85d79b5b2bbc1a8c8c7c120dadb658671659723d
arickert <45617293+arickert@users.noreply.github.com>
2023-10-17 16:31:53 -0700

    Update config.js

--- _site/tina/config.js
+++ _site/tina/config.js
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master"
 
 export default defineConfig({
   branch,
-  clientId: "9c28524f-cd54-4cb7-b7f6-3cc0fcda37d3", // Get this from tina.io
-  token: "247d3c3a1c3e00bb1e2539a13763c6037692b7ee", // Get this from tina.io
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID, 
+  token: process.env.TINA_TOKEN,
 
   build: {
     outputFolder: "admin",
